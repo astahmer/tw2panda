3eab0a61a808db1e3bf10f2e5f53743f56b2d970
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-07-12 15:49:30 +0200

    chore: rm container

--- packages/web/src/pages/Home.tsx
+++ packages/web/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Container, Flex, HStack, Stack } from "panda/jsx";
+import { Flex, HStack, Stack } from "panda/jsx";
 import { styled } from "panda/jsx";
 import PlaygroundWithMachine from "../Playground/PlaygroundWithMachine";
 
@@ -18,25 +18,24 @@ export const Home = () => {
         color={{ base: "cyan.600", _dark: "cyan.200" }}
         bg={{ base: "whiteAlpha.100", _dark: "whiteAlpha.200" }}
         fontFamily="Inter"
+        p="3"
       >
-        <Container w="100%" h="100%">
-          <Stack w="100%" h="100%">
-            <Flex pt="2">
-              <styled.h1 textStyle="panda.h4" fontWeight="bold">
-                Tailwind to Panda (tw2panda)
-              </styled.h1>
-              <HStack alignItems="center" ml="auto">
-                <styled.a target="blank" href="https://github.com/astahmer/tw2panda">
-                  <IconButton title="Github">
-                    <GithubIcon />
-                  </IconButton>
-                </styled.a>
-                <ColorModeSwitch />
-              </HStack>
-            </Flex>
-            <PlaygroundWithMachine />
-          </Stack>
-        </Container>
+        <Stack w="100%" h="100%">
+          <Flex pt="2">
+            <styled.h1 textStyle="panda.h4" fontWeight="bold">
+              Tailwind to Panda (tw2panda)
+            </styled.h1>
+            <HStack alignItems="center" ml="auto">
+              <styled.a target="blank" href="https://github.com/astahmer/tw2panda">
+                <IconButton title="Github">
+                  <GithubIcon />
+                </IconButton>
+              </styled.a>
+              <ColorModeSwitch />
+            </HStack>
+          </Flex>
+          <PlaygroundWithMachine />
+        </Stack>
       </Flex>
     </ThemeProvider>
   );
