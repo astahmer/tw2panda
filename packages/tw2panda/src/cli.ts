bac019b09731e2cbd7cabc81344877c80a0aa6df
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-07-21 18:08:37 +0200

    maybe this works now ?

--- packages/tw2panda/src/cli.ts
+++ packages/tw2panda/src/cli.ts
@@ -56,7 +56,6 @@ cli
     const { mergeCss } = createMergeCss(Object.assign(panda, { hash: false }));
 
     const result = rewriteTwFileContentToPanda(content, tw.context, panda, mergeCss, options as RewriteOptions);
-    //
     if (options.write) {
       return await writeFile(join(cwd, file), result.output);
     }
