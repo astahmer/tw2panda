dbd29c656d563a3308df4b2d641c096a138f6998
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-08-08 15:24:30 +0200

    fix(dev): fs.shim missing some exports

--- packages/web/fs.shim.ts
+++ packages/web/fs.shim.ts
@@ -1,3 +1,9 @@
+export const stat = (path: string) => {
+  return {
+    isFile: () => true,
+  };
+};
+
 export const statSync = (path: string) => {
   return {
     isFile: () => true,
@@ -5,6 +11,10 @@ export const statSync = (path: string) => {
 };
 
 export const readdirSync = (path: string) => {
+  return [];
+};
+
+export const readdir = (path: string) => {
   return [];
 };
 
